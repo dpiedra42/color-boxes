e469b8d8c502c812e2af9c24562d9cebaa91ea2d
deanna <42.dpiedra@gmail.com>
2021-11-04 23:33:56 +0100

    can now see one square of a set color (pink)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Box from './Box';
 import './App.css';
 
 class App extends Component{
   render() {
     return (
       <div>
-        <h1>hi</h1>
+        <Box />
       </div>
     );
   }
--- src/Box.js
+++ src/Box.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import './Box.css'
 
 class Box extends Component{
-  render() {
-    return (
-      <div>
-        <h1>box</h1>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = { color : 'pink' };
+    }
+    render() {
+        return (
+            <div className="Box" style={{backgroundColor: this.state.color}}>
+            </div>
+        );
+    }
 } 
 
 export default Box;
